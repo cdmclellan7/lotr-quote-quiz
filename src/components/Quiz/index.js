0cd2e99e03d851fb8e440fff457acd6287c8999a
casey <cassandra.d.mclellan@gmail.com>
2022-01-16 14:56:02 +0000

    FEAT: Quiz displays the quote and four choices for character, one of which is correct

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,19 +1,67 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 import Quote from "../Quote";
 
+const GET_NEW_QUIZ = "GET_NEW_QUIZ";
+
 function Quiz({ quotes, characters, movies }) {
+	/*
 	const [currentQuote, setCurrentQuote] = useState(null);
+	const [currentMultipleChoice, setCurrentMultipleChoice] = useState();
+    */
+
+	const [currentQuizData, dispatch] = useReducer(quizReducer, null);
+
+	function quizReducer(state, action) {
+		switch (action.type) {
+			case GET_NEW_QUIZ:
+				const currentQuote = getRandomQuote();
+				const answer = getCharacterNameFromId(currentQuote.character);
+				const allChoices = getRandomChoices(answer);
+				return { currentQuote, answer, allChoices };
+			default:
+				return state;
+		}
+	}
 
 	function getRandomQuote() {
 		const randomNum = Math.floor(Math.random() * quotes.length);
-		setCurrentQuote(quotes[randomNum]);
+		return quotes[randomNum];
+	}
+
+	function getRandomCharacter() {
+		const randomNum = Math.floor(Math.random() * characters.length);
+		return characters[randomNum];
+	}
+
+	function getCharacterNameFromId(id) {
+		const character = characters.find((character) => character.id === id);
+		return character.name;
+	}
+
+	function getRandomChoices(name) {
+		const choices = [{ name: name, isCorrect: true }];
+		while (choices.length < 4) {
+			let newCharacterName = getRandomCharacter().name;
+			if (!choices.includes(newCharacterName)) {
+				choices.push({ name: newCharacterName, isCorrect: false });
+			}
+		}
+		return choices;
 	}
 
 	return (
 		<div id="quiz">
-			{currentQuote && <Quote text={currentQuote.dialog} />}
-			<button onClick={getRandomQuote}>Get New Quote</button>
+			{currentQuizData && (
+				<Quote text={currentQuizData.currentQuote.dialog} />
+			)}
+			{currentQuizData &&
+				currentQuizData.allChoices.map((choice) => {
+					return <p key={choice.name}>{choice.name}</p>;
+				})}
+			<button onClick={() => dispatch({ type: GET_NEW_QUIZ })}>
+				Get New Quote
+			</button>
 		</div>
 	);
 }
