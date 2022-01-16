1ed68cfa7c01edbbe77b7581f50c84fc8b984cbd
casey <cassandra.d.mclellan@gmail.com>
2022-01-16 09:18:11 +0000

    CHORE: added prettier config file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the LOTR Movie Quote Quiz</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>
+				Welcome to the
+				LOTR Movie Quote
+				Quiz
+			</h1>
+		</div>
+	);
 }
 
 export default App;
