0a56a0a45963c212c5ecebaa2d0059f0fced39ce
casey <cassandra.d.mclellan@gmail.com>
2022-01-16 15:16:37 +0000

    FEAT: MultipleChoice displays the choices sorted alphabetically

--- src/components/MultipleChoice/index.js
+++ src/components/MultipleChoice/index.js
@@ -1,8 +1,31 @@
 function MultipleChoice({ allChoices }) {
+	function makeSortedOrder(arr) {
+		return arr.sort((a, b) => {
+			const nameA = a.name.toUpperCase();
+			const nameB = b.name.toUpperCase();
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+	}
+
 	return (
 		<div id="multiple-choice">
-			{allChoices.map((choice) => {
-				return <p key={choice.name}>{choice.name}</p>;
+			{makeSortedOrder(allChoices).map((choice) => {
+				return (
+					<p
+						key={choice.name}
+						style={{
+							backgroundColor: choice.isCorrect ? "green" : "red",
+						}}
+					>
+						{choice.name}
+					</p>
+				);
 			})}
 		</div>
 	);
