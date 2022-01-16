a00d96d53033e582631be68a6310c3f746233e71
casey <cassandra.d.mclellan@gmail.com>
2022-01-16 12:25:08 +0000

    FEAT: app fetches book data from the api

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
+	const [quoteData, setQuoteData] = useState(null);
+
+	useEffect(() => {
+		async function fetchQuoteData() {
+			const data = await fetch(
+				"https://the-one-api.dev/v2/book"
+			);
+			const json = await data.json();
+
+			setQuoteData(json);
+		}
+
+		fetchQuoteData();
+	}, []);
 	return (
 		<div className="App">
 			<h1>
@@ -8,6 +24,11 @@ function App() {
 				LOTR Movie Quote
 				Quiz
 			</h1>
+			<p>
+				{JSON.stringify(
+					quoteData
+				)}
+			</p>
 		</div>
 	);
 }
